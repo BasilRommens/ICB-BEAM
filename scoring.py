352dc717923677aa4446bd12aaf10ee78661ebbb
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2021-05-05 18:45:12 +0200

    Scoring matrix generation

--- scoring.py
+++ scoring.py
@@ -1,5 +1,6 @@
 # Help functions for scoring a motif matrix
 from copy import copy
+from math import log
 
 BASES = ["A", "T", "C", "G"]
 
@@ -39,12 +40,98 @@ def count_to_frequency_matrix(count_matrix):
     return frequency_matrix
 
 
-if __name__ == '__main__':
+def score_pssm(string, scoring_matrix):
+    """
+    Scores a DNA string using given scoring_matrix (slide 19)
+    Motif length of scoring matrix must be the same as the string length
+    :param string: DNA string, consist only of letters A, T, C or G
+    :param scoring_matrix: Dict containing for every base (A, T, C and G) a list with the relative frequency of the base in a position
+    :return: Rating between 0 (lowest match) and 1 (highest match)
 
-    from pprint import pprint
+    >>> score_pssm("AT", {'A': [1.0, 0.0], 'T': [0.0, 0.5], 'C': [0.0, 0.5], 'G': [0.0, 0.0]})
+    0.5
+    """
+    score = 1
+    for i in range(len(string)):
+        base = string[i]
+        score *= scoring_matrix[base][i]
+    return score
 
-    instances = ["TATTAAAA", "AATAAATA", "TACAAATA", "TTTAAGAA", "TATACATA"]
+
+def score_pssm_log(string, log_matrix):
+    """
+    Scores a DNA string using given scoring_matrix which is logged (slide 21)
+    Lower score is better
+    """
+    score = 0
+    for i in range(len(string)):
+        base = string[i]
+        score += log_matrix[base][i]
+    return score
+
+
+def add_pseudo_counts(frequency_matrix, low_frequency=0.1):
+    """
+    Replaces all zeros with a low frequency
+    >>> add_pseudo_counts({'A': [1.0, 0.0], 'T': [0.0, 0.5], 'C': [0.0, 0.5], 'G': [0.0, 0.0]})
+    {'A': [1.0, 0.1], 'T': [0.1, 0.5], 'C': [0.1, 0.5], 'G': [0.1, 0.1]}
+    """
+    pseudo_matrix = dict()
+    for base, occurances in frequency_matrix.items():
+        # TODO: Substract from other numbers, so sum for each position is still 1
+        # zero_count = sum(map(lambda i: i == 0, occurances))
+        # nonzero_count = len(BASES)-zero_count
+        # diff = (zero_count * low_frequency) / nonzero_count
+        # pseudo_matrix[base] = [low_frequency if i == 0 else i for i in occurances]
+        pseudo_matrix[base] = [low_frequency if i == 0 else i for i in occurances]
+    return pseudo_matrix
+
+
+def freq_to_log_matrix(frequency_matrix):
+    """
+    Takes the log of all elements in the matrix
+    Note: Pseudocounts must be added!!! (else log 0 -> error)
+    """
+    log_matrix = dict()
+    for base, occurances in frequency_matrix.items():
+        log_matrix[base] = [-log(i) for i in occurances]
+    return log_matrix
+
+
+def get_scoring_matrix(instances):
     count_matrix = instances_to_count_matrix(instances)
     frequency_matrix = count_to_frequency_matrix(count_matrix)
+    pseudo_matrix = add_pseudo_counts(frequency_matrix)
+    log_matrix = freq_to_log_matrix(pseudo_matrix)
+    return log_matrix
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+
+    print("Count Matrix")
+    instances = ["TATTAAAA", "AATAAATA", "TACAAATA", "TTTAAGAA", "TATACATA"]
+    count_matrix = instances_to_count_matrix(instances)
     pprint(count_matrix)
+
+    print("Frequency Matrix")
+    frequency_matrix = count_to_frequency_matrix(count_matrix)
     pprint(frequency_matrix)
+
+    print("Scoring PSSM")
+    score1 = score_pssm("TATACATA", frequency_matrix)
+    score2 = score_pssm("GCATCATT", frequency_matrix)
+    score3 = score_pssm("TATAAAAT", frequency_matrix)
+    print(f"scores: {score1}, {score2} and {score3}")
+
+    print("Pseudocounts")
+    pseudo_matrix = add_pseudo_counts(frequency_matrix)
+    pprint(pseudo_matrix)
+
+    print("Log Matrix")
+    log_matrix = freq_to_log_matrix(pseudo_matrix)
+    pprint(log_matrix)
+    score1 = score_pssm_log("TATACATA", log_matrix)
+    score2 = score_pssm_log("GCATCATT", log_matrix)
+    score3 = score_pssm_log("TATAAAAT", log_matrix)
+    print(f"scores: {score1}, {score2} and {score3}")
